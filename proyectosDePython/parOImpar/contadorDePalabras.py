cb28483f0781b6175b100e9b09fd27847685ddd0
Rav <92896807+Rav-s@users.noreply.github.com>
2022-02-04 01:17:37 -0300

    comentado

--- proyectosDePython/parOImpar/contadorDePalabras.py
+++ proyectosDePython/parOImpar/contadorDePalabras.py
@@ -1,2 +1,4 @@
+# declramos variable con texto a contar
 texto = "hola kabros programadores"
+# con el metodo count contamos la cantidadad de espacios y le sumamos uno por la primera palabra
 print(texto.count(" ") + 1)
