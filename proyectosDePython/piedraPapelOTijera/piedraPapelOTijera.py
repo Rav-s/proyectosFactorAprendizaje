38ddff9efdd4bc19bfb9aed96a255a8e5ee933a9
Rav <92896807+Rav-s@users.noreply.github.com>
2022-02-05 00:51:59 -0300

    piedra papel o tijera terminado

--- proyectosDePython/piedraPapelOTijera/piedraPapelOTijera.py
+++ proyectosDePython/piedraPapelOTijera/piedraPapelOTijera.py
@@ -1,4 +1,30 @@
+# definimos que vamos opcion vamos a seleccionar 
+entrada = input("¿piedra, papel o tijera? ")
 # importamos random para sacar número aleatorio
 from random import randrange
+# sacamos un numero aleatorio para la lista de nombres
 numeroDeListado = randrange(1,3)
+# hacer una lista de nombres (piedra ,papel o tijera)
+lista = ["piedra","papel","tijera"]
+# definimos variable(con el número aleatorio) para compararla más tarde 
+salidaDeRobot = lista[numeroDeListado]
+# ponemos un bloque condicional. con el que ponemos por ejemplo que si el robot da piedra y el usuario papel se imprima ganaste
+if salidaDeRobot == "piedra" and entrada == "papel":
+    print("ganaste")
+if salidaDeRobot == "papel" and entrada == "tijera":
+    print("ganaste")
+if salidaDeRobot == "tijera" and entrada == "piedra":
+    print("ganaste")
 
+# aqui lo mismo solo que al reves
+if entrada == "piedra" and salidaDeRobot == "papel":
+    print("perdiste")
+if entrada == "papel" and salidaDeRobot == "tijera":
+    print("perdiste")
+if entrada == "tijera" and salidaDeRobot == "piedra":
+    print("perdiste")
+# y aqui ponemos un condicional con el que ponemos que si entrada (usuario) es igual que la maqina poner empate
+if entrada == salidaDeRobot:
+    print("empate")
+# aqui pintamos en terminal que puso la maquina
+print(salidaDeRobot)
