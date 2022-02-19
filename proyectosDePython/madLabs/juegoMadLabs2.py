2bec2e4dafee2a5dae39ce9b96ea4413d48757cd
Rav <92896807+Rav-s@users.noreply.github.com>
2022-02-18 23:37:30 -0300

    actualización

--- proyectosDePython/madLabs/juegoMadLabs2.py
+++ proyectosDePython/madLabs/juegoMadLabs2.py
@@ -1,23 +1,2 @@
-persona1 = input("persona 1 ")
-persona2 = input("persona 2 ")
-persona3 = input("persona 3 ")
-persona4 = input("persona 4 ")
-persona5 = input("persona 5 ")
-persona6 = input("persona 6 ")
-persona7 = input("persona 7 ")
-persona8 = input("persona 8 ")
-persona9 = input("persona 9 ")
-persona10 = input("persona 10 ")
-
-carac1 = input("persona 1 ")
-carac2 = input("persona 2 ")
-carac3 = input("persona 3 ")
-carac4 = input("persona 4 ")
-carac5 = input("persona 5 ")
-carac6 = input("persona 6 ")
-carac7 = input("persona 7 ")
-carac8 = input("persona 8 ")
-carac9 = input("persona 9 ")
-carac10 = input("persona 10 ")
 
 
---
+++ whatsapp/botDeWhatsapp.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.sendwhatmsg("+56990715252", "hola mundo",00, 1)
