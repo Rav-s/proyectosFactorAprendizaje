edd8ae8feac4798b10a3bfcec037daddb9c97cd3
Rav <92896807+Rav-s@users.noreply.github.com>
2022-03-12 20:47:23 -0300

    Create qr2.py

---
+++ proyectosDePython/proyectosSimples/qr/qr2.py
@@ -0,0 +1,8 @@
+from matplotlib.pyplot import fill
+import qrcode
+link = 'hola mundo'
+qr = qrcode.QRCode(version=1,box_size=10,border=5)
+qr.add_data(link)
+qr.make(fit=True)
+img = qr.make_image()
+img.save('QRLINK.png')
